5e294d55aba3ef7f3b869b692e6a2e8d6317223b
Dionicio <floresdionicio681@gmail.com>
2026-06-09 12:00:10 -0600

    Implemented Secure the vault workshop

---
+++ index.js
@@ -0,0 +1,7 @@
+let code1;
+code1 = 10;
+let code2 = 150 / 3 - 10;
+let code3 = 139 % 50;
+let message = "The vault has been secured. The combination is: ";
+let codeA = `${code1}-${code2}-${code3}`;
+console.log(codeA);
